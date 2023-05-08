961d5e0c8a1f934f5b9c00f2f92dc87c7ec85dea
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-08 13:00:31 -0500

    feat: add parcelable in data class

--- app/src/main/java/com/example/musicpie2/model/Song.kt
+++ app/src/main/java/com/example/musicpie2/model/Song.kt
@@ -1,3 +1,33 @@
 package com.example.musicpie2.model
 
-data class Song(var cover: Int, var songTitle: String, var songArtist: String, var audio: Int)
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Song(val cover: Int, val songTitle: String, val songArtist: String, val audio: Int) :
+    Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readInt()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(cover)
+        parcel.writeString(songTitle)
+        parcel.writeString(songArtist)
+        parcel.writeInt(audio)
+    }
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Song> {
+        override fun createFromParcel(parcel: Parcel): Song {
+            return Song(parcel)
+        }
+        override fun newArray(size: Int): Array<Song?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
