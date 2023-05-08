23de195e0e936800b8e2f296d65aad4c96db5261
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-08 13:59:05 -0500

    feat: load initial playlist

--- app/src/main/java/com/example/musicpie2/view/ui/MainActivity.kt
+++ app/src/main/java/com/example/musicpie2/view/ui/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.musicpie2.view.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.musicpie2.R
 import com.example.musicpie2.databinding.ActivityMainBinding
+import com.example.musicpie2.model.PlaylistSingleton
+import com.example.musicpie2.model.Song
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +14,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        loadPlaylist()
+    }
+
+    private fun loadPlaylist() {
+        PlaylistSingleton.playlist.add(Song(R.drawable.cover1, "Title 1", "Artist 1", R.raw.song1))
+        PlaylistSingleton.playlist.add(Song(R.drawable.cover2, "Title 2", "Artist 2", R.raw.song2))
+        PlaylistSingleton.playlist.add(Song(R.drawable.cover3, "Title 3", "Artist 3", R.raw.song3))
     }
 }
