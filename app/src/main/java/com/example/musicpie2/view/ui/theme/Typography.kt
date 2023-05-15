44db3c0ea0c15f099619bc1e79e5ca954fba0dd1
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-15 13:36:09 -0500

    feat: add typography

---
+++ app/src/main/java/com/example/musicpie2/view/ui/theme/Typography.kt
@@ -0,0 +1,55 @@
+package com.example.musicpie2.view.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.musicpie2.R.font
+
+val NotoSerif = FontFamily(
+    Font(font.noto_serif_regular, FontWeight.Normal),
+    Font(font.noto_serif_bold, FontWeight.Bold),
+    Font(font.noto_serif_italic, FontWeight.Light, FontStyle.Italic),
+    Font(font.noto_serif_bold_italic, FontWeight.Bold, FontStyle.Italic)
+)
+
+val typography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = NotoSerif,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = NotoSerif,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp
+    ),
+    bodyLarge = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontFamily = NotoSerif,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontFamily = NotoSerif,
+        fontSize = 10.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.15.sp,
+    ),
+    bodySmall = TextStyle(
+        fontWeight = FontWeight.Light,
+        fontFamily = NotoSerif,
+        fontSize = 6.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.15.sp
+    )
+)
