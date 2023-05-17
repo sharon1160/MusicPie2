977da6ce42b80373f553f0ae537bd859534fa591
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-17 09:08:58 -0500

    feat: update shape dimensions

--- app/src/main/java/com/example/musicpie2/view/ui/theme/Shapes.kt
+++ app/src/main/java/com/example/musicpie2/view/ui/theme/Shapes.kt
@@ -5,7 +5,7 @@ import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val shapes = Shapes(
-    medium = RoundedCornerShape(12.dp),
+    medium = RoundedCornerShape(8.dp),
     large = RoundedCornerShape(16.dp),
     extraLarge = RoundedCornerShape(24.dp)
 )
